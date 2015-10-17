cb15435a36c8e8fa2466023c323325e58aa4063e
Balaji <bala92@gmail.com>
2015-10-17 03:53:52 +0000

    final commit

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -46,8 +46,8 @@ Given /^I am on the RottenPotatoes home page$/ do
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies have been added to RottenPotatoes:/ do |movies_table|
-  pending  # Remove this statement when you finish implementing the test step
   movies_table.hashes.each do |movie|
+    Movie.create(movie)
     # Each returned movie will be a hash representing one row of the movies_table
     # The keys will be the table headers and the values will be the row contents.
     # Entries can be directly to the database with ActiveRecord methods
@@ -56,19 +56,85 @@ Given /the following movies have been added to RottenPotatoes:/ do |movies_table
 end
 
 When /^I have opted to see movies rated: "(.*?)"$/ do |arg1|
+
+    all= Movie.all_ratings
+      all.each do |x|
+        uncheck("ratings_#{x}")
+    end
+
+    arr=arg1.split(',').map!(&:strip)
+    arr.each do |x|
+        check("ratings_#{x}")
+    end
+
+    click_on "ratings_submit"
+    
   # HINT: use String#split to split up the rating_list, then
   # iterate over the ratings and check/uncheck the ratings
   # using the appropriate Capybara command(s)
-  pending  #remove this statement after implementing the test step
 end
 
 Then /^I should see only movies rated: "(.*?)"$/ do |arg1|
-  pending  #remove this statement after implementing the test step
+
+all= Movie.all_ratings
+#arr=arg1.split(',')
+arr=arg1.split(',').map!(&:strip)
+
+arr.each do |i|
+all.delete(i)
+end
+
+puts all
+puts arr
+
+
+#rat = page.all('table#rodents td').map(&:text)
+
+
+ result = false
+ all("tbody tr").each do |tr|
+     arr.each do |i|
+        puts tr.text 
+        if tr.has_content?(i)
+          #puts i
+          result = true
+        end
+     end
+end   
+#  all("tr").each do |tr|
+#      all.each do |i|
+#      if tr.has_content?(i)
+#       result = false
+#      end
+#  end
+#   end 
+  expect(result).to be_truthy
+
 end
 
 Then /^I should see all of the movies$/ do
-  pending  #remove this statement after implementing the test step
+  #pending  #remove this statement after implementing the test step
+resut=false
+
+rows=page.all("#movies tr").size-1
+if (rows==Movie.count)
+result=true
+end
+expect(result).to be_truthy
+
 end
 
 
 
+When /^I have opted to sort: "(.*?)"$/ do |arg1|
+   click_on arg1
+end
+   
+Then /^I should see "(.*?)" before "(.*?)"$/ do |e,f|
+result=false
+#puts page.body
+if (page.body =~ /#{e}.+#{f}/m)
+    result=true
+end
+expect(result).to be_truthy
+end
